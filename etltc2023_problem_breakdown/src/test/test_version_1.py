bf68b9e7ef0a415bb5a7f9667409dd9dd88f1798
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-03-19 01:24:13 +0900

    ✅ add| added pytest unit tests.

--- etltc2023_problem_breakdown/src/test/test_version_1.py
+++ etltc2023_problem_breakdown/src/test/test_version_1.py
@@ -1,7 +1,7 @@
 from main.version_1 import describe_difference
 
 
-def test_describe_difference():
+def test_describe_difference() -> None:
     assert describe_difference(1, 1) == "remain the same"
-    assert describe_difference(1.8, 1.7) == "decrease"
-    assert describe_difference(51.22, 51.23) == "increase"
+    assert describe_difference(8, 7) == "decrease"
+    assert describe_difference(5122, 5123) == "increase"
