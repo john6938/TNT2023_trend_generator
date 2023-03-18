ef0ed6d031663759eeef0c2a20d7a8df009aaebb
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-03-18 13:52:49 +0900

    🚚 move| moved python files created in 2022.

---
+++ project/2022_tasks/src/test/test_task3_func.py
@@ -0,0 +1,23 @@
+from app.task3_func import (
+    append_date_dif,
+    append_min_max,
+    append_verb,
+    convert_to_past_tense,
+    create_sentence,
+    should_connect_two_sentences,
+    show_change_strength,
+    show_days_from_today,
+    summerize_sentence,
+)
+
+
+def test_connect_two_sentences():
+    assert should_connect_two_sentences(1, 1) == True
+    assert should_connect_two_sentences(1, 2) == False
+    assert should_connect_two_sentences(-2, 2) == False
+
+
+def test_append_min_max():
+    assert append_min_max(15.5, "The number ") == "The number peaked"
+    assert append_min_max(3.11, "The price ") == "The price hit a low"
+    assert append_min_max(7.54, "The best record of malathon ") == "The best record of malathon "
